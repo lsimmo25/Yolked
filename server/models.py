0cdf5f753f788cb32b77051b9fbff8d24d7497db
Lyle Simmons <lylesimmons125@gmail.com>
2024-07-08 19:49:15 -0400

    added validators to exercise and workoutexercise

--- server/models.py
+++ server/models.py
@@ -2,18 +2,18 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
-
 from config import db, bcrypt
 
-class User(db.model, SerializerMixin):
-
+class User(db.Model, SerializerMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Integer)
+    username = db.Column(db.String, nullable=False)
     _password_hash = db.Column(db.String, nullable=True)
     image_url = db.Column(db.String, nullable=True)
     bio = db.Column(db.String, nullable=True)
+
+    workouts = db.relationship('Workout', back_populates='user', cascade='all, delete-orphan')
 
     @validates('username')
     def validate_username(self, key, username):
@@ -35,33 +35,39 @@ class User(db.model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
-class Workout(db.model, SerializerMixin):
-    
+class Workout(db.Model, SerializerMixin):
     __tablename__ = "workouts"
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.Date, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='workouts')
     exercises = db.relationship('WorkoutExercise', back_populates='workout')
 
-class Exercise(db.model, SerializerMixin):
-
+class Exercise(db.Model, SerializerMixin):
     __tablename__ = "exercises"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
 
     workouts = db.relationship('WorkoutExercise', back_populates='exercise')
 
-class WorkoutExercise(db.Model, SerializerMixin):
+    @validates('name')
+    def validates_name(self, key, name):
+        if not name:
+            raise ValueError('validation errors')
+        return name
 
+
+class WorkoutExercise(db.Model, SerializerMixin):
     __tablename__ = "workout_exercises"
 
     id = db.Column(db.Integer, primary_key=True)
-    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'), nullable=False)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
     weight = db.Column(db.Float, nullable=False)
     reps = db.Column(db.Integer, nullable=False)
 
-    exercise = db.relationship('Exercise', back_populates='workout_exercises')
-
+    workout = db.relationship('Workout', back_populates='exercises')
+    exercise = db.relationship('Exercise', back_populates='workouts')
